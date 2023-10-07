7df38cc7b4e6373245700ed9c66a4f5f31809174
daniel jermaine <danieljermaine1@gmail.com>
2023-10-07 10:27:25 +0100

    Initial Commit

---
+++ BetBudd/BetBuddApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetBuddApp.swift
+//  BetBudd
+//
+//  Created by MacBook AIR on 07/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct BetBuddApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

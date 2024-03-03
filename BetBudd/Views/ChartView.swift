a6a6462e31322cb749ce16ad1d8c51e5d072d473
BigJermaine <113020989+bigjermaine@users.noreply.github.com>
2024-03-03 07:54:07 +0100

    added the chartView

--- BetBudd/Views/ChartView.swift
+++ BetBudd/Views/ChartView.swift
@@ -5,14 +5,55 @@
 //  Created by MacBook AIR on 03/03/2024.
 //
 
-import SwiftUI
 
+import SwiftUI
+import SwiftUICharts
+import CoreData
 struct ChartView: View {
+  
+    @EnvironmentObject var bettingViewModel:BettingSaveManager
+    @State private var totalAmountSpent:  [Double] = []
+    var demoData: [Double] = [8.00, 2.00, 4, 6, 12.00, 9.00, 2.00]
+    var sum: Double = 0.00
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack{
+            if  !totalAmountSpent.isEmpty {
+                LineView(data: totalAmountSpent, title: "BettingExpenseChart")
+                    .padding(.horizontal)
+                Spacer()
+                
+                LineChartView(data: totalAmountSpent, title: "Exp")
+            }else {
+                VStack{
+                    Text("Add some Transactions,to display graph")
+                        .bold()
+                        .fontWeight(.semibold)
+                }
+            }
+            
+        }
+        .padding(.horizontal,8)
+        .onAppear{
+            addTotal()
+        }
+        
+    }
+    
+    func addTotal() {
+        for transaction in bettingViewModel.bettingEntities.map({$0.amount}) {
+            let totalAmountSpent = transaction?.toDouble() ?? 0.00
+            self.totalAmountSpent.append(totalAmountSpent)
+        }
+        
+        
+        
     }
 }
 
-#Preview {
-    ChartView()
+struct ChartView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChartView()
+    }
 }

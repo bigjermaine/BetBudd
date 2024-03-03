0298b5ecae4f5c2ccc49ef848c3d293d5edd7271
BigJermaine <113020989+bigjermaine@users.noreply.github.com>
2024-03-03 07:43:29 +0100

    added ChartView

---
+++ BetBudd/Views/ChartView.swift
@@ -0,0 +1,18 @@
+//
+//  ChartView.swift
+//  BetBudd
+//
+//  Created by MacBook AIR on 03/03/2024.
+//
+
+import SwiftUI
+
+struct ChartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChartView()
+}
